8d279588f00e9d2cf1ef72f65bfcd8f5bb7e4550
Felicity Graham <Felicity.rose.graham@outlook.com>
2023-05-17 00:52:23 -0700

    drawings and re-enabling btns

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 let currentword = '';
+let lettersGuessed = '';
+let missedGuesses = 0;
+let playingGame = false;
+let wordBank = [];
 
-const words = ['able', 'about', 'account', 'acid', 'across', 'act', 'addition', 'adjustment', 
+const words = ['able', 'about', 'account', 'acid', 'across', 'act', 'addition', 'advertisement',
                 'advertisement', 'after', 'again', 'against', 'agreement', 'air', 'all', 'almost', 
                 'among', 'amount', 'amusement', 'and', 'angle', 'angry', 'animal', 'answer', 'ant', 
-                'any', 'apparatus', 'apple', 'approval', 'arch', 'argument', 'arm', 'army', 'art', 
-                'as', 'at', 'attack', 'attempt', 'attention', 'attraction', 'authority', 'automatic', 
+                'any', 'apparatus', 'apple', 'approval', 'arch', 'argument', 'arm', 'army', 'adjustment', 
+                'attack', 'attempt', 'attention', 'attraction', 'authority', 'automatic',  'art', 
                 'awake', 'baby', 'back', 'bad', 'bag', 'balance', 'ball', 'band', 'base', 'basin', 
-                'basket', 'bath', 'be', 'beautiful', 'because', 'bed', 'bee', 'before', 'behaviour',
+                'basket', 'bath', 'beautiful', 'because', 'bed', 'bee', 'before', 'behaviour',
                 'belief', 'bell', 'bent', 'berry', 'between', 'bird', 'birth', 'bit', 'bite', 'bitter',
                 'black', 'blade', 'blood', 'blow', 'blue', 'board', 'boat', 'body', 'boiling', 'bone', 
                 'book', 'boot', 'bottle', 'box', 'boy', 'brain', 'brake', 'branch', 'brass', 'bread', 
                 'breath', 'brick', 'bridge', 'bright', 'broken', 'brother', 'brown', 'brush', 'bucket', 
-                'building', 'bulb', 'burn', 'burst', 'business', 'but', 'butter', 'button', 'by', 'cake',
+                'building', 'bulb', 'burn', 'burst', 'business', 'but', 'butter', 'button', 'cake',
                 'camera', 'canvas', 'card', 'care', 'carriage', 'cart', 'cat', 'cause', 'certain', 'chain', 
                 'chalk', 'chance', 'change', 'cheap', 'cheese', 'chemical', 'chin', 'church', 'circle', 
                 'clean', 'clear', 'clock', 'cloth', 'cloud', 'coal', 'coat', 'cold', 'collar', 'colour', 
@@ -22,7 +26,7 @@ const words = ['able', 'about', 'account', 'acid', 'across', 'act', 'addition', 
                 'damage', 'danger', 'dark', 'daughter', 'day', 'dead', 'dear', 'death', 'debt', 'decision', 
                 'deep', 'degree', 'delicate', 'dependent', 'design', 'desire', 'destruction', 'detail', 
                 'development', 'different', 'digestion', 'direction', 'dirty', 'discovery', 'discussion', 
-                'disease', 'disgust', 'distance', 'distribution', 'division', 'do', 'dog', 'door', 'doubt',
+                'disease', 'disgust', 'distance', 'distribution', 'division', 'dog', 'door', 'doubt',
                 'down','drain', 'drawer', 'dress', 'drink', 'driving', 'drop', 'dry', 'dust', 'ear', 'early', 
                 'earth', 'east', 'edge', 'education', 'effect', 'egg', 'elastic', 'electric', 'end', 'engine', 
                 'enough', 'equal', 'error', 'even', 'event', 'ever', 'every', 'example', 'exchange', 'existence', 
@@ -34,10 +38,10 @@ const words = ['able', 'about', 'account', 'acid', 'across', 'act', 'addition', 
                 'fruit', 'full', 'future', 'garden', 'general', 'get', 'girl', 'give', 'glass', 'glove', 
                 'go', 'goat', 'gold', 'good', 'government', 'grain', 'grass', 'great', 'green', 'grey',
                 'grip', 'group', 'growth', 'guide', 'gun', 'hair', 'hammer', 'hand','hanging', 'happy', 
-                'harbour', 'hard', 'harmony', 'hat', 'hate', 'have', 'he', 'head', 'healthy', 'hear', 
+                'harbour', 'hard', 'harmony', 'hat', 'hate', 'have', 'head', 'healthy', 'hear',
                 'hearing', 'heart', 'heat', 'help', 'high', 'history', 'hole', 'hollow', 'hook', 'hope', 
-                'horn', 'horse', 'hospital', 'hour', 'house', 'how', 'humour', 'ice', 'idea', 'if', 'ill', 
-                'important', 'impulse', 'in', 'increase', 'industry', 'ink', 'insect', 'instrument', 'insurance', 
+                'horn', 'horse', 'hospital', 'hour', 'house', 'how', 'humour', 'ice', 'idea', 'ill', 
+                'important', 'impulse', 'increase', 'industry', 'ink', 'insect', 'instrument', 'insurance', 
                 'interest', 'invention', 'iron', 'island', 'jelly', 'jewel', 'join', 'journey', 'judge', 
                 'jump', 'keep', 'kettle', 'key', 'kick', 'kind', 'kiss', 'knee', 'knife', 'kno', 'knowledge', 
                 'land', 'language', 'last', 'late', 'laugh', 'law', 'lead', 'leaf', 'learning', 'leather', 
@@ -48,10 +52,10 @@ const words = ['able', 'about', 'account', 'acid', 'across', 'act', 'addition', 
                 'memory', 'metal', 'middle', 'military', 'milk', 'mind', 'mine', 'minute', 'mist', 'mixed', 
                 'money', 'monkey', 'month', 'moon', 'morning', 'mother', 'motion', 'mountain', 'mouth', 
                 'move', 'much', 'muscle', 'music', 'nail', 'name', 'narrow', 'nation', 'natural', 'near', 
-                'necessary', 'neck', 'need', 'needle', 'nerve', 'net', 'new', 'news', 'night', 'no', 'noise', 
+                'necessary', 'neck', 'need', 'needle', 'nerve', 'net', 'new', 'news', 'night', 'noise', 
                 'normal', 'north', 'nose', 'not', 'note', 'now', 'number', 'nut', 'observation', 'of', 'off', 
-                'offer', 'office', 'oil', 'old', 'on', 'only', 'open', 'operation', 'opinion', 'opposite', 
-                'or', 'orange', 'order', 'organization', 'ornament', 'other', 'out', 'oven', 'over', 'owner', 
+                'offer', 'office', 'oil', 'old', 'only', 'open', 'operation', 'opinion', 'opposite', 
+                'orange', 'order', 'organization', 'ornament', 'other', 'out', 'oven', 'over', 'owner', 
                 'page', 'pain', 'paint', 'paper', 'parallel', 'parcel', 'part', 'past', 'paste', 'payment', 
                 'peace', 'pen', 'pencil', 'person', 'physical', 'picture', 'pig', 'pin', 'pipe', 'place', 
                 'plane', 'plant', 'plate', 'play', 'please', 'pleasure', 'plough', 'pocket', 'point', 'poison', 
@@ -68,7 +72,7 @@ const words = ['able', 'about', 'account', 'acid', 'across', 'act', 'addition', 
                 'serious', 'servant', 'shade', 'shake', 'shame', 'sharp', 'sheep', 'shelf', 'ship', 
                 'shirt', 'shock', 'shoe', 'short', 'shut', 'side', 'sign', 'silk', 'silver', 'simple', 
                 'sister', 'size', 'skin', 'skirt', 'sky', 'sleep', 'slip', 'slope', 'slow', 'small', 'smash', 
-                'smell', 'smile', 'smoke', 'smooth', 'snake', 'sneeze', 'snow', 'so', 'soap', 'society', 
+                'smell', 'smile', 'smoke', 'smooth', 'snake', 'sneeze', 'snow', 'soap', 'society', 
                 'sock', 'soft', 'solid', 'some', 'son', 'song', 'sort', 'sound', 'soup', 'south', 'space', 
                 'spade', 'special', 'sponge', 'spoon', 'spring', 'square', 'stage', 'stamp', 'star', 'start', 
                 'statement', 'station', 'steam', 'steel', 'stem', 'step', 'stick', 'sticky', 'stiff', 
@@ -76,17 +80,16 @@ const words = ['able', 'about', 'account', 'acid', 'across', 'act', 'addition', 
                 'strange', 'street', 'stretch', 'strong', 'structure', 'substance', 'such', 'sudden', 
                 'sugar', 'suggestion', 'summer', 'sun', 'support', 'surprise', 'sweet', 'swim', 'system', 
                 'table', 'tail', 'take', 'talk', 'tall', 'taste', 'tax', 'teaching', 'tendency', 'test', 
-                'than', 'that', 'the', 'then', 'theory', 'there', 'thick', 'thin', 'thing', 'this', 'thought', 
-                'thread', 'throat', 'through', 'thumb', 'thunder', 'ticket', 'tight', 'till', 'time', 
-                'tin', 'tired', 'to', 'toe', 'together', 'tomorrow', 'tongue', 'tooth', 'top', 'touch', 
-                'town', 'trade', 'train', 'transport', 'tray', 'tree', 'trick', 'trouble', 'trousers',
-                'turn', 'twist', 'umbrella', 'under', 'unit', 'up', 'use', 'value', 'verse', 'very', 
-                'vessel', 'view', 'violent', 'voice', 'waiting', 'walk', 'wall', 'war', 'warm', 'wash', 
-                'waste', 'watch', 'water', 'wave', 'wax', 'way', 'weather', 'week', 'weight', 'well', 
-                'west', 'wet', 'wheel', 'when', 'where', 'while', 'whip', 'whistle', 'white', 'who', 
-                'why', 'wide', 'will', 'wind', 'window', 'wine', 'wing', 'winter', 'wire', 'wise', 'with', 
+                'theory', 'there', 'thick', 'thin', 'thing', 'this', 'thought', 'thread', 'throat', 'through',
+                'thumb', 'thunder', 'ticket', 'tight', 'till', 'time', 'tin', 'tired', 'toe', 'together', 
+                'tomorrow', 'tongue', 'tooth', 'top', 'touch', 'town', 'trade', 'train', 'transport', 'tray',
+                'tree', 'trick', 'trouble', 'trousers', 'turn', 'twist', 'umbrella', 'under', 'unit', 'use', 
+                'value', 'verse', 'very', 'vessel', 'view', 'violent', 'voice', 'waiting', 'walk', 'wall', 
+                'war', 'warm', 'wash', 'waste', 'watch', 'water', 'wave', 'wax', 'way', 'weather', 'week',
+                'weight', 'well', 'west', 'wet', 'wheel', 'when', 'where', 'while', 'whip', 'whistle', 'white', 
+                'who', 'why', 'wide', 'will', 'wind', 'window', 'wine', 'wing', 'winter', 'wire', 'wise', 'with', 
                 'woman', 'wood', 'wool', 'word', 'work', 'worm', 'wound', 'writing', 'wrong', 'year', 
-                'yellow', 'yes', 'yesterday', 'you', 'young', 'android'];
+                'yellow', 'yes', 'yesterday', 'young', 'android'];
 
 document.addEventListener('DOMContentLoaded', e => {
     drawGallow();
@@ -164,11 +167,50 @@ const drawRightLeg = () => {
         context.stroke();
 }
 const drawHappyFace = () => {
-
+     const canvas = document.getElementById('player-gallow');
+     const context = canvas.getContext('2d');
+   //semicircle for smile
+    context.beginPath();
+    context.strokeStyle = 'black';
+    context.lineWidth = 3;
+    context.arc(200, 50, 20, 0, Math.PI, false);
+    context.stroke();
+    //draw eyes 
+    context.beginPath();
+    context.fillStyle = "green"; // color
+    context.arc(190, 45, 3, 0, Math.PI * 2, true); // left eye
+    context.fill();
+    context.arc(210, 45, 3, 0, Math.PI * 2, true); // right eye
+    context.fill();
 };
-
+ 
 const drawDeadFace = () => {
+    const canvas = document.getElementById('player-gallow');
+    const context = canvas.getContext('2d');
+    //semicircle for frown
+    context.beginPath();
+    context.strokeStyle = 'black';
+    context.lineWidth = 3;
+    context.arc(200, 70, 10, 0, Math.PI, true);
+    context.stroke();
+    // eyes 
+    context.beginPath();
+    context.strokeStyle = 'red';
+    context.lineWidth = 3;
+
+    // left eye
+    context.moveTo(195, 40);
+    context.lineTo(185, 50);
+    context.moveTo(185, 40);
+    context.lineTo(195, 50);
     
+    //right eye
+    context.moveTo(215, 40);
+    context.lineTo(205, 50);
+    context.moveTo(205, 40);
+    context.lineTo(215, 50);
+
+    context.stroke();
 }
 
 const drawGallow = () => {
@@ -218,6 +260,48 @@ const drawLetters = (word) => {
     }
 };
 
+const btnLetterClick = (letterBeingPassedIn) => { 
+    if (!playingGame) {
+        return;
+    }
+    console.log(letterBeingPassedIn);
+    let mybutton = document.getElementById(`btn${letterBeingPassedIn}`)
+    mybutton.disabled = true;
+
+    const foundLetter = currentword.includes(letterBeingPassedIn);
+    if (foundLetter) {
+
+    } else {
+        let badguess = 0;
+        badguess++;
+        if (badguess == 1) {
+            drawHead();
+        }
+        else if (badguess == 2) {
+            drawBody();
+        }else if (badguess ==3) {
+            drawLeftArm();
+        }else if (badguess ==4) {
+            drawRightArm();
+        }else if (badguess ==5) {
+            drawLeftLeg();
+        }else if (badguess ==6) {
+            drawRightLeg();
+        } else {
+            drawDeadFace();
+        }
+    }
+
+        if (badguess ==7) {
+            document.getElementById('lblMessege').innerText = 'You lost! Click New Game button to play again';
+        } else if (areAllLettersGuessed) {
+               drawHappyFace(); //drawhappyface
+               //game is over set messege to "you win"
+        }
+
+};
+
+
 const reEnableLetterBtns = () => {
 
 };
@@ -230,4 +314,20 @@ const startNewGame = () => {
     drawGallow();
     drawLetters(currentword);
     reEnableLetterBtns();
+    playingGame = true;
+    missedGuesses = 0;
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    for (let index = 0; index < alphabet.length; index++) {
+        document.getElementById(`btn${alphabet[index]}`).disabled = false;   
+    }
+};
+
+const areAllLettersGuessed = () => {
+    for (let i = 0; i < currentword.length - 1; i++); {
+        const currentwordletter = currentword[i];
+        if (!lettersGuessed.includes(currentwordletter)) {
+        return false;
+     }
+    } 
+        return true;
 };
